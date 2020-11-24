bbffc7d83f3116393ea1d6e72fa8731f718adb13
Abhishek Verma <av4programming@gmail.com>
2020-11-24 13:36:41 +0530

    done

--- src/commands/scheduler.js
+++ src/commands/scheduler.js
@@ -18,20 +18,18 @@ function counter(bot) {
     let input = ctx.message.text.toLowerCase();
     let inputArray = input.split(" ");
     if (inputArray.length === 2 && inputArray[1] == "r") {
-      ctx.session.counter = 0;
       bot.context.counter = 0;
       return ctx.reply("counter reset successfully");
     } else if (
       inputArray.length === 3 &&
-      inputArray[1] == "d" &&
-      typeof inputArray[2] == "number"
+      inputArray[1] == "s" &&
+      typeof parseInt(inputArray[2]) == "number"
     ) {
       bot.context.counter = inputArray[2];
-      ctx.session.counter = bot.context.counter;
-      return ctx.reply(`counter set to ${ctx.session.counter}`);
+      return ctx.reply(`counter set to ${bot.context.counter}`);
     }
-    ctx.session.counter = bot.context.counter || 0;
-    ctx.reply(ctx.session.counter);
+    bot.context.counter = bot.context.counter || 0;
+    ctx.reply(bot.context.counter);
   });
 }
 
