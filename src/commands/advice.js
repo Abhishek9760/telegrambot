c5d37d568333aaa01c6c879b9cbcd5f53d5edb8d
Abhishek Verma <av4programming@gmail.com>
2021-03-01 17:20:05 +0530

    done

--- src/commands/advice.js
+++ src/commands/advice.js
@@ -1,10 +1,11 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = (bot) => {
-    bot.command('advice', ctx =>{
-        axios.get('https://api.adviceslip.com/advice').then(f = (res) => {
-            ctx.reply(res.data.slip.advice);
-        });
-    
-    });
-}
+  bot.command(["advice", "advice@cutio_bot"], (ctx) => {
+    axios.get("https://api.adviceslip.com/advice").then(
+      (f = (res) => {
+        ctx.reply(res.data.slip.advice);
+      })
+    );
+  });
+};
